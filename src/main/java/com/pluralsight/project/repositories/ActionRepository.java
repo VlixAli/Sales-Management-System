7a6f8412679e87959ae7199efe11dd4b13bf887f
Ali <qali372@gmail.com>
2024-04-05 14:36:57 +0200

    fix : some method names

--- src/main/java/com/pluralsight/project/repositories/ActionRepository.java
+++ src/main/java/com/pluralsight/project/repositories/ActionRepository.java
@@ -13,14 +13,14 @@ public interface ActionRepository extends JpaRepository<Action,Long> {
     List<Action> findByApplicationName(String name);
     List<Action> findByUserLastNameAndApplicationName(String lastName,String ApplicationNme);
     List<Action> findByUserLastNameAndApplicationNameAndBeName(String lastName,String ApplicationNme,String BEName);
-    List<Action> findByActionTypeNameEng(String name);
-    List<Action> findByUserLastNameAndActionTypeNameEng(String name,String ATName);
-    List<Action> findByUserLastNameAndApplicationNameAndBeNameAndActionTypeNameEng(String lastName,String ApplicationNme,String BEName,String ActionTypeName);
+    List<Action> findByActionTypeNameEn(String name);
+    List<Action> findByUserLastNameAndActionTypeNameEn(String name,String ATName);
+    List<Action> findByUserLastNameAndApplicationNameAndBeNameAndActionTypeNameEn(String lastName,String ApplicationNme,String BEName,String ActionTypeName);
     List<Action> findByParamsValue(String paramValue);
     List<Action> findByParamsValueAndParamsParamTypeNameEn(String value,String name);
-    List<Action> findByUserLastNameAndApplicationNameAndBeNameAndActionTypeNameEngAndParamsValueAndParamsParamTypeNameEn(String lastName, String ApplicationNme, String BEName, String ActionTypeName,String ParamValue,String PTName);
+    List<Action> findByUserLastNameAndApplicationNameAndBeNameAndActionTypeNameEnAndParamsValueAndParamsParamTypeNameEn(String lastName, String ApplicationNme, String BEName, String ActionTypeName,String ParamValue,String PTName);
     List<Action> findByUserLastNameAndParamsValueAndParamsParamTypeNameEn(String name,String PValue,String PTName);
-    List<Action> findByUserLastNameAndActionTypeNameEngAndParamsValueAndParamsParamTypeNameEn(String name,String ATName,String PValue,String PTName);
+    List<Action> findByUserLastNameAndActionTypeNameEnAndParamsValueAndParamsParamTypeNameEn(String name,String ATName,String PValue,String PTName);
 
 
 }
