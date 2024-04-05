8a8124be8ed6e8632ac85375f95de8ef07837fc3
Ali <qali372@gmail.com>
2024-04-05 14:42:33 +0200

    build : ParamRequest.java

--- src/main/java/com/pluralsight/project/dtos/requests/ParamRequest.java
+++ src/main/java/com/pluralsight/project/dtos/requests/ParamRequest.java
@@ -4,12 +4,9 @@ import com.pluralsight.project.models.ParamType;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 @Data
 @NoArgsConstructor
-public class ParamTypeRequest {
-    private String paramId ;
-    private String paramValue ;
-    private ActionRequest action;
-    private ParamType paramType;
+public class ParamRequest {
+    private String value;
+    private Long paramType;
 }
