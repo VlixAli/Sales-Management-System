37ef6ddb07be309e7d6a800731063c4d74e21870
Ali <qali372@gmail.com>
2024-04-05 14:40:09 +0200

    fix : remove dtoRequest mapping from ActionMapper.java and ParamMapper.java

--- src/main/java/com/pluralsight/project/mappers/ActionMapper.java
+++ src/main/java/com/pluralsight/project/mappers/ActionMapper.java
@@ -1,21 +1,19 @@
 package com.pluralsight.project.mappers;
 
 import com.pluralsight.project.dtos.responses.ActionResponse;
-import com.pluralsight.project.dtos.requests.ActionRequest;
 import com.pluralsight.project.models.Action;
 import org.mapstruct.Mapper;
-
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ActionMapper {
+
     List<ActionResponse> listActionResponse(List<Action> actions);
 
+    @Mapping(target = "actionId", source = "action.id")
     ActionResponse actionToActionResponse(Action action);
 
-    Action actionRequestToAction(ActionRequest actionRequest);
-
-    Action actionResponseToAction(ActionResponse actionResponse);
 
 }
--- src/main/java/com/pluralsight/project/mappers/ParamMapper.java
+++ src/main/java/com/pluralsight/project/mappers/ParamMapper.java
@@ -1,6 +1,5 @@
 package com.pluralsight.project.mappers;
 
-import com.pluralsight.project.dtos.requests.ParamTypeRequest;
 import com.pluralsight.project.dtos.responses.ParamResponse;
 import com.pluralsight.project.models.Param;
 import org.mapstruct.Mapper;
@@ -11,7 +10,6 @@ import java.util.List;
 public interface ParamMapper {
 
     List<ParamResponse> listParamResponse(List<Param> params);
-    List<Param> listParam(List<ParamResponse> paramResponses);
     ParamResponse ParamToParamResponse(Param param);
     Param ParamResponseToParam(ParamResponse paramResponse);
 
