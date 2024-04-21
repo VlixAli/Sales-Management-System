b9aba03c7a0d59dfa69671b1bac3b7272979ef2e
Ali <qali372@gmail.com>
2024-04-21 09:57:51 +0200

    feat : add email , password, role to User.java

---
+++ src/main/java/com/pluralsight/project/models/Role.java
@@ -0,0 +1,6 @@
+package com.pluralsight.project.models;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/pluralsight/project/models/User.java
+++ src/main/java/com/pluralsight/project/models/User.java
@@ -1,15 +1,24 @@
 package com.pluralsight.project.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 import java.util.List;
 
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -20,7 +29,43 @@ public class User {
     @Column(name = "last_name")
     private String lastName;
 
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Action> actions;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
