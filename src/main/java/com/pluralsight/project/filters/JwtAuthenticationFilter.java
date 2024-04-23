30f5217ddb74a72941745cfdbaa64e0ec6fdd77a
Ali <qali372@gmail.com>
2024-04-23 15:23:10 +0200

    feat : check if the token is valid in the database as well as in the jwt itself in JwtAuthenticationFilter.java

--- src/main/java/com/pluralsight/project/filters/JwtAuthenticationFilter.java
+++ src/main/java/com/pluralsight/project/filters/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.pluralsight.project.filters;
 
+import com.pluralsight.project.repositories.TokenRepository;
 import com.pluralsight.project.services.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -23,6 +24,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private final TokenRepository tokenRepository;
 
     @Override
     protected void doFilterInternal(
@@ -40,7 +42,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         userEmail = jwtService.extractUsername(jwt);
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-            if (jwtService.isTokenValid(jwt, userDetails)) {
+            var isTokenValid = tokenRepository.findByToken(jwt)
+                    .map(t -> !t.isExpired() && !t.isRevoked())
+                    .orElse(false);
+            if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
                         null,
