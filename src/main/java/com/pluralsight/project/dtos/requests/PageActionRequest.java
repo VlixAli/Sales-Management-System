9325aa9fd7524da8bff663993ce96de0830524d5
Ali <qali372@gmail.com>
2024-04-24 20:44:33 +0200

    feat : validate if sort and sortByColumn are in the allowed values in PageActionRequest.java

--- src/main/java/com/pluralsight/project/dtos/requests/PageActionRequest.java
+++ src/main/java/com/pluralsight/project/dtos/requests/PageActionRequest.java
@@ -1,5 +1,6 @@
 package com.pluralsight.project.dtos.requests;
 
+import com.pluralsight.project.validations.annotations.InValues;
 import lombok.Data;
 import org.springframework.data.domain.Sort;
 
@@ -7,16 +8,32 @@ import org.springframework.data.domain.Sort;
 public class PageActionRequest {
 
     private String username;
+
     private String be;
+
     private String application;
+
     private Long traceId;
+
     private String param;
 
     private String paramTypeEn;
 
+
     private Integer pageNo;
 
-    private Sort.Direction sort;
+    @InValues(value = {"ASC", "DESC"}, message = "the value must be either ASC or DESC")
+    private String sort;
 
+    @InValues(value = {"id", "descriptionAr", "descriptionEn", "actionTime", "traceId"},
+            message = "the value must be in [id, descriptionAr, descriptionEn, actionTime, traceId]")
     private String sortByColumn;
+
+    public Sort.Direction sortToDirection(String value) {
+        if (value == "DESC") {
+            return Sort.Direction.DESC;
+        } else {
+            return Sort.Direction.ASC;
+        }
+    }
 }
