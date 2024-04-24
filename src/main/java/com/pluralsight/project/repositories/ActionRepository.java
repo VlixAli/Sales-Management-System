2ef6330f3c982403d7e82727d255344df7f49aac
Ali <qali372@gmail.com>
2024-04-24 20:50:10 +0200

    build : findByTraceId() in ActionRepository.java

--- src/main/java/com/pluralsight/project/repositories/ActionRepository.java
+++ src/main/java/com/pluralsight/project/repositories/ActionRepository.java
@@ -5,7 +5,10 @@ import com.pluralsight.project.models.Action;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 
 public interface ActionRepository extends JpaRepository<Action, Long>, JpaSpecificationExecutor<Action> {
 
+    Optional<Action> findByTraceId(String traceId);
 }
