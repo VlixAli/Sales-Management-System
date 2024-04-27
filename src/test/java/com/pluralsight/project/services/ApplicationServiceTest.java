9e1a02708f5a372a1470249bc8f03c0a1db29072
Ali <qali372@gmail.com>
2024-04-27 21:34:14 +0300

    feat : add unit tests to ApplicationService create() method

--- src/test/java/com/pluralsight/project/services/ApplicationServiceTest.java
+++ src/test/java/com/pluralsight/project/services/ApplicationServiceTest.java
@@ -1,16 +1,20 @@
 package com.pluralsight.project.services;
 
+import com.pluralsight.project.dtos.requests.ApplicationRequest;
 import com.pluralsight.project.mappers.ApplicationMapper;
+import com.pluralsight.project.models.Application;
 import com.pluralsight.project.repositories.ApplicationRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -47,5 +51,22 @@ class ApplicationServiceTest {
         verify(applicationMapper).listApplicationResponse(new ArrayList<>());
     }
 
+    @Test
+    void createUsesApplicationRepositorySave(){
+        //when
+        underTest.create(any());
+
+        //then
+        verify(applicationRepository).save(any());
+    }
+
+    @Test
+    void createUsesApplicationMapperApplicationRequestToApplication(){
+        //when
+        underTest.create(any());
+
+        //then
+        verify(applicationMapper).applicationRequestToApplication(any());
+    }
 
 }
