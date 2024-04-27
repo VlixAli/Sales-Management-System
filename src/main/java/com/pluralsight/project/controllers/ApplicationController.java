04ef2b31a72811deabd684fcc1f3ca595c8db0b1
Ali <qali372@gmail.com>
2024-04-27 21:33:45 +0300

    feat : create an applications endpoint

--- src/main/java/com/pluralsight/project/controllers/ApplicationController.java
+++ src/main/java/com/pluralsight/project/controllers/ApplicationController.java
@@ -1,11 +1,13 @@
 package com.pluralsight.project.controllers;
 
+import com.pluralsight.project.dtos.requests.ApplicationRequest;
+import com.pluralsight.project.dtos.responses.ApplicationResponse;
 import com.pluralsight.project.services.ApplicationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/applications")
@@ -18,4 +20,12 @@ public class ApplicationController {
     public ResponseEntity<?> index(){
         return ResponseEntity.ok(applicationService.findAll());
     }
+
+    @PostMapping
+    public ResponseEntity<?> store(@RequestBody @Validated ApplicationRequest applicationRequest){
+        ApplicationResponse applicationResponse = applicationService.create(applicationRequest);
+        return new ResponseEntity<>(applicationResponse, HttpStatus.CREATED);
+    }
+
+
 }
--- src/main/java/com/pluralsight/project/services/ApplicationService.java
+++ src/main/java/com/pluralsight/project/services/ApplicationService.java
@@ -1,5 +1,6 @@
 package com.pluralsight.project.services;
 
+import com.pluralsight.project.dtos.requests.ApplicationRequest;
 import com.pluralsight.project.dtos.responses.ApplicationResponse;
 import com.pluralsight.project.mappers.ApplicationMapper;
 import com.pluralsight.project.models.Application;
@@ -16,8 +17,16 @@ public class ApplicationService {
     private final ApplicationRepository applicationRepository;
     private final ApplicationMapper applicationMapper;
 
-    public List<ApplicationResponse> findAll(){
+    public List<ApplicationResponse> findAll() {
         return applicationMapper.listApplicationResponse(applicationRepository.findAll());
+    }
+
+    public ApplicationResponse create(ApplicationRequest applicationRequest) {
+        Application application =
+                applicationMapper.applicationRequestToApplication(applicationRequest);
+
+        applicationRepository.save(application);
+        return applicationMapper.applicationToApplicationResponse(application);
     }
 
 }
