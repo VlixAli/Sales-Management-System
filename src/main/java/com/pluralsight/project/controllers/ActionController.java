3fc088202a73e36c8f73d185f59d2d9a2d61052f
Ali <qali372@gmail.com>
2024-04-30 08:02:49 +0300

    refactor : remove the exception handling logic from ActionController and move it to the GlobalExceptionHandler

--- src/main/java/com/pluralsight/project/controllers/ActionController.java
+++ src/main/java/com/pluralsight/project/controllers/ActionController.java
@@ -4,7 +4,6 @@ import com.pluralsight.project.dtos.requests.ActionRequest;
 import com.pluralsight.project.dtos.requests.PageActionRequest;
 import com.pluralsight.project.dtos.requests.PageRequestDto;
 import com.pluralsight.project.dtos.responses.ActionResponse;
-import com.pluralsight.project.dtos.responses.ErrorResponse;
 import com.pluralsight.project.services.ActionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -14,13 +13,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("api/v1/actions")
 @RequiredArgsConstructor
 public class ActionController {
-
-    private final ResponseEntity<ErrorResponse> errorResponse =
-            new ResponseEntity<>(new ErrorResponse("Action not found"), HttpStatus.NOT_FOUND);
 
     private final ActionService actionService;
 
@@ -37,33 +36,29 @@ public class ActionController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> show(@PathVariable Long id) {
+    public ResponseEntity<ActionResponse> show(@PathVariable Long id) {
         ActionResponse actionResponse = actionService.findById(id);
-        if (actionResponse == null) {
-            return errorResponse;
-        }
         return ResponseEntity.ok(actionResponse);
     }
 
     @PostMapping
-    public ResponseEntity<?> store(@RequestBody @Validated(ActionRequest.Save.class) ActionRequest actionRequest) {
+    public ResponseEntity<ActionResponse> store(@RequestBody @Validated(ActionRequest.Save.class) ActionRequest actionRequest) {
         ActionResponse actionResponse = actionService.create(actionRequest);
         return new ResponseEntity<>(actionResponse, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> update(@PathVariable Long id,
-                                 @RequestBody @Validated(ActionRequest.Update.class) ActionRequest actionRequest) {
+    public ResponseEntity<ActionResponse> update(@PathVariable Long id,
+                                                 @RequestBody @Validated(ActionRequest.Update.class) ActionRequest actionRequest) {
         ActionResponse actionResponse = actionService.update(id, actionRequest);
-        if (actionResponse == null) {
-            return errorResponse;
-        }
         return ResponseEntity.ok(actionResponse);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<HttpStatus> delete(@PathVariable Long id) {
+    public ResponseEntity<?> delete(@PathVariable Long id) {
         actionService.delete(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Action deleted successfully");
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
