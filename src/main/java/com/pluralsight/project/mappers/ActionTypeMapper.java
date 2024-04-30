50e37778c67972808a9f6dd678e16a50f5702eac
Ali <qali372@gmail.com>
2024-04-30 07:38:22 +0300

    build : crud operations endpoints for ActionType

--- src/main/java/com/pluralsight/project/mappers/ActionTypeMapper.java
+++ src/main/java/com/pluralsight/project/mappers/ActionTypeMapper.java
@@ -5,8 +5,12 @@ import com.pluralsight.project.dtos.responses.ActionTypeResponse;
 import com.pluralsight.project.models.ActionType;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface ActionTypeMapper {
+
+    List<ActionTypeResponse> listActionTypeResponse(List<ActionType> actionTypes);
 
     ActionTypeResponse actionTypeToATResponse(ActionType actionType);
 
